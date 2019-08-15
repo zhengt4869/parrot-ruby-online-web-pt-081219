3e17863bcf5b46593800272c8147c18cf442841d
Tianlin Zheng <zhengt@mit.edu>
2019-08-15 01:54:00 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,10 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+<<<<<<< HEAD
 def parrot(phrase = "Squawk!")
   puts phrase
+=======
+def parrot(phrase = "Squawk")
+>>>>>>> 7d7ef09b7d48bf5407404f9532246e9b21a5fc9f
   return phrase 
 end
